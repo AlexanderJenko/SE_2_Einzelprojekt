9f926accf14f2c5d333798203756fc35325028ec
Alexander Jenko <alexander.jenko@gmail.com>
2026-03-12 22:06:44 +0100

    100% coverage LeaderboardController

--- src/test/kotlin/at/aau/serg/controllers/LeaderboardControllerTests.kt
+++ src/test/kotlin/at/aau/serg/controllers/LeaderboardControllerTests.kt
@@ -57,4 +57,38 @@ class LeaderboardControllerTests {
         assertEquals(third, res.body!![2])
     }
 
+    // Testen des "rank" Parameters
+    @Test
+    fun test_getLeaderboard_invalidRank_returnsBadRequest() {
+        whenever(mockedService.getGameResults()).thenReturn(listOf(GameResult(1, "p", 10, 10.0)))
+
+        val resTooSmall = controller.getLeaderboard(0)
+        assertEquals(HttpStatus.BAD_REQUEST, resTooSmall.statusCode)
+
+        val resTooLarge = controller.getLeaderboard(2)
+        assertEquals(HttpStatus.BAD_REQUEST, resTooLarge.statusCode)
+    }
+
+    // Sublisten Test
+    @Test
+    fun test_getLeaderboard_validRank_returnsSublist() {
+        val p1 = GameResult(1, "p1", 100, 10.0)
+        val p2 = GameResult(2, "p2", 90, 10.0)
+        val p3 = GameResult(3, "p3", 80, 10.0)
+        val p4 = GameResult(4, "p4", 70, 10.0)
+        val p5 = GameResult(5, "p5", 60, 10.0)
+        val p6 = GameResult(6, "p6", 50, 10.0)
+        val p7 = GameResult(7, "p7", 40, 10.0)
+        val p8 = GameResult(8, "p8", 30, 10.0)
+
+        whenever(mockedService.getGameResults()).thenReturn(listOf(p1, p2, p3, p4, p5, p6, p7, p8))
+
+        val res = controller.getLeaderboard(4)
+
+        assertEquals(HttpStatus.OK, res.statusCode)
+        assertEquals(7, res.body!!.size)
+        assertEquals(p1, res.body!![0])
+        assertEquals(p7, res.body!![6])
+    }
+
 }
